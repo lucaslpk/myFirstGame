7fb64d169c5038241587585b685d726ccd375e8b
Lucas Lpk <lukasz.lepkowski@gmail.com>
2023-03-15 10:33:58 +0000

    added keep track if standing and which way facing

--- engine.py
+++ engine.py
@@ -25,19 +25,40 @@ class player(object) : # need to read more and understand why is there an object
         self.left = False         # these 3 vars will keep track of which way and how many steps is the character walking, for accurate image insertions
         self.right = False
         self.walkCount = 0 
+        self.standing = True
 
     def draw(self, win) :
         if self.walkCount + 1 >= 27 :        # every 27 steps reset counter or risk index error, as len(walkLeft/Right)=9 and img is to change every 3 frames @ 27 fps
             self.walkCount = 0
-        if self.left :
-            win.blit(walkLeft[self.walkCount//3],(self.x,self.y))
-            self.walkCount += 1
-        elif self.right :
-            win.blit(walkRight[self.walkCount//3], (self.x,self.y))
-            self.walkCount += 1
+
+        if not(self.standing) :
+            if self.left :
+                win.blit(walkLeft[self.walkCount//3],(self.x,self.y))
+                self.walkCount += 1
+            elif self.right :
+                win.blit(walkRight[self.walkCount//3], (self.x,self.y))
+                self.walkCount += 1
+
         else :
-            win.blit(char,(self.x,self.y))
-            self.walkCount = 0
+            self.walkCount = 0      # so that fram No 0 is blit when standing w/o hardcoding 0 in the procedures below
+            if self.left :
+                win.blit(walkLeft[self.walkCount//3],(self.x,self.y))       # same procedures as above, for consistency
+            elif self.right :
+                win.blit(walkRight[self.walkCount//3], (self.x,self.y))
+            else :              # this wil blit our thug facing screen if both self.left/right are false, which is the case only at the beginning of the game, any other time he stops, we will be facing in the last direction that he walked
+                win.blit(char,(self.x,self.y))
+
+class projectile() :
+    def __init__(self,x,y,radius,color, facing) -> None:
+        self.radius = radius
+        self.x = x
+        self.y = y
+        self.color = color 
+        self.facing = facing    # facing Left/Right will be coded as 1 or -1
+        self.v = 15 * facing    # so that the velocity will be a vector and will have and orientation (+/-)
+
+    def draw(self, win) :
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius, 1)   # jedynka jst żeby nie był wypełniony, puste dla pełny - zobaczymy
 
 def redrawGameWin() :
     win.blit(bg, (0,0))
@@ -60,17 +81,17 @@ while run :
     if keys[pygame.K_LEFT] and thug.x > 0:
         thug.x -= thug.v   
         thug.left = True
-        thug.right = False     
+        thug.right = False  
+        thug.standing = False   
     elif keys[pygame.K_RIGHT] and thug.x < screen_W - thug.w:
         thug.x += thug.v 
         thug.left = False
         thug.right = True
-    else :              # if neither left nor right key pressed - stand still and reset walkCount
-        thug.left = False
-        thug.right = False
+        thug.standing = False
+    else :              # if neither left nor right key pressed - stand still and reset walkCount 
         thug.walkCount = 0 
-    if not thug.isJump :     # up, down and new jump will be suspended while previous jump lasts
-        if keys[pygame.K_SPACE] :
+    if not thug.isJump :     # up, down and new jump will be suspended while previous jump lasts Edit: up/down was removed, for platform game.
+        if keys[pygame.K_UP] :
             thug.isJump = True  # TwTim is adding here left = right = False & reset walk count so the thug doesn't move feet while in the air
     else :
         if thug.jumpCount >= -10 :
